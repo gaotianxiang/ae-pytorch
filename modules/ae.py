b3b0ff3ee564d79fc003cace0ca56c93cbd20da5
Tianxiang Gao <gtx9726@gmail.com>
2019-04-06 10:13:22 -0400

    ae done

---
+++ modules/ae.py
@@ -0,0 +1,82 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+
+class Encoder(nn.Module):
+    def __init__(self, hidden_size):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.conv1_1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1)
+        self.conv1_2 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, padding=1)
+        self.conv2_1 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
+        self.conv2_2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1)
+        self.conv3_1 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.conv3_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
+        self.fcn = nn.Linear(in_features=1024, out_features=hidden_size)
+
+    def forward(self, imgs):
+        x = imgs
+        x = self.conv1_1(x)
+        x = F.relu(x)
+        x = self.conv1_2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=2)
+
+        x = F.relu(self.conv2_1(x))
+        x = F.relu(self.conv2_2(x))
+        x = F.max_pool2d(x, kernel_size=2)
+
+        x = F.relu(self.conv3_1(x))
+        x = F.relu(self.conv3_2(x))
+        x = F.max_pool2d(x, kernel_size=2)
+
+        x = x.view(x.size(0), -1)
+        x = self.fcn(x)
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self, hidden_size):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.fcn = nn.Linear(in_features=hidden_size, out_features=1024)
+        self.conv3_1 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
+        self.conv3_2 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1)
+        self.conv2_1 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1)
+        self.conv2_2 = nn.Conv2d(in_channels=32, out_channels=16, kernel_size=3, padding=1)
+        self.conv1_1 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, padding=1)
+        self.conv1_2 = nn.Conv2d(in_channels=16, out_channels=3, kernel_size=3, padding=1)
+
+    def forward(self, imgs):
+        x = imgs
+        x = self.fcn(x)
+        x = x.view(x.size(0), 4, 4, 64)
+
+        x = F.upsample_bilinear(x, scale_factor=2)
+        x = self.conv3_1(x)
+        x = self.conv3_2(x)
+
+        x = F.upsample_bilinear(x, scale_factor=2)
+        x = self.conv2_1(x)
+        x = self.conv2_2(x)
+
+        x = F.upsample_bilinear(x, scale_factor=2)
+        x = self.conv1_1(x)
+        x = self.conv1_2(x)
+
+        return x
+
+
+class AutoEncoder(nn.Module):
+    def __init__(self, hidden_size):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.encoder = Encoder(hidden_size)
+        self.decoder = Decoder(hidden_size)
+
+    def forward(self, imgs):
+        x = imgs
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
